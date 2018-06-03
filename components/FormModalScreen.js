3d07d1b6973c09171c4f94314dc10bac772c9182
Mel Chua <mchuam@gmail.com>
2018-06-02 20:29:19 -0700

    added changes to add a model to navigation. We are now using nested navigation.

--- components/FormModalScreen.js
+++ components/FormModalScreen.js
@@ -23,11 +23,7 @@ class FormModalScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-        <Button
-          onPress={() => this.props.navigation.goBack()}
-          title="Dismiss"
-        />
+        <Text onPress={()=> this.props.navigation.goBack()}>Back</Text>
       </View>
     );
   }

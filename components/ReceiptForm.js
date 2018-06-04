e0696695de5234efdfea446cda74589dd293d34d
billywoo17 <billwu_@outlook.com>
2018-06-03 17:07:30 -0700

    reached MVP

--- components/ReceiptForm.js
+++ components/ReceiptForm.js
@@ -1,35 +1,71 @@
 import React, { Component } from 'react';
-import { Text, StatusBar, TextInput, View, StyleSheet } from 'react-native';
+import { Text, StatusBar, TextInput, View, StyleSheet, Picker } from 'react-native';
 import { Constants } from 'expo';
 import { List, ListItem, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
+import { Dropdown } from 'react-native-material-dropdown';
 
 
 
 export default class ReceiptFormModal extends Component {
- state = {
-   total: '',
-   date: '',
-   category: '',
-   location: '',
- };
+  constructor(props){
+    super(props),
+    state = {
+      total: '',
+      date: '',
+      category: '',
+      location: '',
+      id:"",
+      project_name:"Lighthouse",
+      description:""
+    };
+  }
+  componentWillMount() {
+    this.setState({ 
+      total: this.props.navigation.state.params.visionResponse.total.toFixed(2).toString(),
+      date: this.props.navigation.state.params.visionResponse.date,
+      id: this.props.navigation.state.params.visionResponse.id,
 
-onSubmitButtonPress() {
- console.log(
-   this.state.total,
-   this.state.date,
-   this.state.category,
-   this.state.location,
-   )
+    });
+  }
+
+getCatId =(category_name) =>{
+
 }
 
+submitForm = () => {
+  console.log("my current states",this.state)
+  console.log("my current props", this.props)
+  fetch('http://10.30.31.122:8080/receipts/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user_id: this.state.id,
+      location: this.state.location,
+      total: this.state.total,
+      date: this.state.date,
+      description: this.state.description,
+      project_id: 1,
+      category_id: 1,
+      image_url: "http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg"
+    })
+  })
+    .then((response) => {
+      this.props.navigation.navigate('Camera')
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
  render() {
-
-   const autoTotal = '5';
-   const autoDate = '';
-   const autoCategory = '';
-   const autoLocation = '';
-
-
+  let data = [{
+    value: 'Food',
+  }, {
+    value: 'Travel',
+  }, {
+    value: 'Entertainment',
+  }];
    return (
      <View style={styles.container}>
        <StatusBar barStyle="light-content" />
@@ -37,30 +73,39 @@ onSubmitButtonPress() {
 
        <FormLabel>Total</FormLabel>
        <FormInput
-       value={autoTotal}
+       value={(this.state.total)}
        placeholder={'Please enter your total'}
        onChangeText = {(inputTotal) => this.setState({total:inputTotal})} />
+
        <FormLabel>Date</FormLabel>
        <FormInput
-       value={autoDate}
+       value={this.state.date}
        placeholder={'MM/DD/YYYY'}
        onChangeText = {(inputDate) => this.setState({date:inputDate})}/>
-       <FormLabel>Category
-
-       </FormLabel>
+       
+       <Dropdown
+       containerStyle = {{padding:20}}
+       label='Category'
+       data={data}
+       onChangeText = {(inputCategory) => this.setState({category:inputCategory})}/>
+       
        <FormLabel>Location</FormLabel>
        <FormInput
-       value={autoLocation}
        placeholder={'Please enter your location'}
        onChangeText = {(inputLocation) => this.setState({location:inputLocation})}/>
+       
+       <FormLabel>Description</FormLabel>
+       <FormInput
+       placeholder={'Please enter your Description'}
+       onChangeText = {(inputDescription) => this.setState({description:inputDescription})}/>
+       
        <Button
          large
          icon={{name: 'squirrel', type: 'octicon', buttonStyle: {backgroundColor: 'black'}}}
          title='SUBMIT'
          buttonStyle={styles.submitButton}
-         onPress={() => this.onSubmitButtonPress()}
-          />
-
+         onPress={this.submitForm.bind(this)}
+          />      
      </View>
    );
  }
